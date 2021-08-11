89ead8dcb33667af2db22bdccf2f448f68d46e53
tuladhp <tuladhp@amazon.com>
2021-08-11 07:15:22 +0200

    refactored test classes

--- test/dms-replication.test.ts
+++ test/dms-replication.test.ts
@@ -3,12 +3,35 @@ import * as cdk from '@aws-cdk/core';
 import '@aws-cdk/assert/jest';
 import assert = require('assert');
 import * as ec2 from '@aws-cdk/aws-ec2';
-import { DMSTestStack } from './stacks/dms-test-stack';
+import * as dms from '@aws-cdk/aws-dms';
+import { DMSProps, DMSReplication } from '../lib/dms-replication';
 import { SynthUtils } from '@aws-cdk/assert';
 import { ContextProps, TaskSettings } from '../lib/context-props';
 import * as Dms from '../lib/dms-stack';
 
 let stack: DMSTestStack;
+
+export class DMSTestStack extends cdk.Stack {
+  public dmsReplication: DMSReplication;
+  private props: DMSProps;
+
+  constructor(scope: cdk.App, id: string) {
+    super(scope, id, { env: { region: 'us-east-1' } });
+
+    const vpc = new ec2.Vpc(this, 'DMSVpc');
+    this.props = {
+      subnetIds: ['subnet-1', 'subnet-2'],
+      replicationInstanceClass: 'dms-mysql-uk',
+      replicationInstanceIdentifier: 'test-repl-01',
+      replicationSubnetGroupIdentifier: 'subnet-group',
+      vpcSecurityGroupIds: ['vpc-security'],
+      engineName: 'mysql',
+      region: 'us-east-1',
+    };
+
+    this.dmsReplication = new DMSReplication(this, 'DMSReplicationService', this.props);
+  }
+}
 
 test('init stack', () => {
   const app = new cdk.App();
